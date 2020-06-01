65ecfe324a14bc4c69b2a7f9bf9f4ea87e1a06d8
ivanardi07 <ardiyanto.ivan07@gmail.com>
2020-06-02 02:23:14 +0700

    project commit

--- app/src/main/java/id/ac/polinema/absenguruprivate/restapi/ApiClient.java
+++ app/src/main/java/id/ac/polinema/absenguruprivate/restapi/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static final String BASE_URL = "https://ivanardiyanto.000webhostapp.com/api/";
+    public static final String BASE_URL = "https://ivanardiyanto07.000webhostapp.com/api/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
